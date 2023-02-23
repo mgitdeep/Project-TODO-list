2a01a18d916e43a109cd37b88e78727c32fed448
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-23 17:15:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
 
 function App() {
   // const name = "Mondeep";
@@ -13,28 +14,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to see changes.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        
+        <Header></Header>
+        {/* <header></header>                 Don't put this small header, put this Header with H in caps*/}
 
-        <p>Hello {handleNameChange()} Developer!</p>
-
-        {/* <p>{"mgitdeep"}</p>
-        <p>{[ 1, 2, 3 ]}</p> */}
-
-        {/* <p>{{ name: 'obj', num: 3 }}</p>
-        <p>{{ a: 1, b: 2 }}</p> */}
-
-        {/* <p>{name}</p> */}
       </header>
       {/* <p>HTML</p> */}
     </div>
