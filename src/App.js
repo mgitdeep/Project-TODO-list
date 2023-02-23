1b47c9b13a4b252a3ebe8571e7242a05dcefc007
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-23 16:40:27 +0530

    Chapter 2

--- src/App.js
+++ src/App.js
@@ -2,31 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = "Mondeep";
+  // const name = "Mondeep";
+  const handleNameChange = () => {
+    const names = [ "Dave", "Ram", "Bob" ];
+    const int = Math.floor(Math.random() * 3);
+
+    return names[int];
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to see changes.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-        <p>Mondeep Codes</p>
+        </a> */}
+
+        <p>Hello {handleNameChange()} Developer!</p>
+
         {/* <p>{"mgitdeep"}</p>
         <p>{[ 1, 2, 3 ]}</p> */}
 
         {/* <p>{{ name: 'obj', num: 3 }}</p>
         <p>{{ a: 1, b: 2 }}</p> */}
 
-        <p>{name}</p>
+        {/* <p>{name}</p> */}
       </header>
       {/* <p>HTML</p> */}
     </div>
