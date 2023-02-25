6d3156afbd31b33d2aeb5f8eecaa747c29650521
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-25 13:47:19 +0530

    update

--- src/Content.js
+++ src/Content.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
 
 const Content = () => {
     const [items, setItems] = useState([
       {
         id: 1,
         checked: false,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        item: " One half pound bag of Cocoa Covered Almonds Unsalted"
       },
       {
         id: 2,
         checked: false,
-        item: "Item 2"
+        item: " Item 2"
       },
       {
         id: 3,
-        checked: false,
-        item: "Item 3"
+        checked: true,
+        item: " Item 3"
       }
     ]);
 
@@ -23,8 +24,11 @@ const Content = () => {
     <main>
       <ul type="none">
         {items.map ((item) => (
-          <li className='item'>
-            <input type="checkbox"  />
+          <li className='item' key={item.id}>
+            <input type="checkbox" checked={item.checked}/>
+            <label>{item.item}</label>
+            {/* <button>Delete</button> */}
+            <FaTrashAlt role={'button'} tabIndex="0" />
           </li>
         ))}
       </ul>
