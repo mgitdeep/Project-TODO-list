a7b1fecb13b5f065bb8858d79a04703c551ab71b
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-25 12:33:21 +0530

    Update

--- src/Content.js
+++ src/Content.js
@@ -1,40 +1,33 @@
 import React, { useState } from 'react'
 
 const Content = () => {
-    const [name, setName] = useState('Dave');
-    const [count, setCount] = useState(0);
-  
-    const handleNameChange = () => {
-        const names = [ "Dave", "Ram", "Bob" ];
-        const int = Math.floor(Math.random() * 3);
-    
-        setName(names[int]);
-    }
-
-    const handleClick = () => {
-      // console.log('You clicked it!')
-      // console.log(count)
-    }
-
-    const handleClick2 = (name) => {
-      console.log(count)
-    }
-
-    const handleClick3 = (e) => {
-      console.log(e.target.innerText)
-    }
+    const [items, setItems] = useState([
+      {
+        id: 1,
+        checked: false,
+        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+      },
+      {
+        id: 2,
+        checked: false,
+        item: "Item 2"
+      },
+      {
+        id: 3,
+        checked: false,
+        item: "Item 3"
+      }
+    ]);
 
   return (
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {name}!
-        </p>
-        <br />
-        <button onClick={handleNameChange}>Change Name</button>
-        <br />
-        <button onClick={handleClick2}>Click It</button>
-        <br />
-        <button onClick={(e) => {handleClick3(e)}}>Click It</button>
+      <ul type="none">
+        {items.map ((item) => (
+          <li className='item'>
+            <input type="checkbox"  />
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
