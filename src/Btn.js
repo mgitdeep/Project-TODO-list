60233c02395e72308d530b33ba70eb22a4736f45
Mondeep Kalita <epicdeep09@gmail.com>
2023-02-28 22:49:26 +0530

    update

--- src/Btn.js
+++ src/Btn.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 function Btn () {
 
-    function handleClick() {
-        console.log("Someone clicked!")
+    // function handleClick() {
+    //     console.log("Someone clicked!")
+    // }
+    // const handleClick = () => {
+    //     console.log("Clickeddd!")
+    // }
+    // return (
+    //     <button onClick={handleClick}>Click ME</button>
+    // )
+    const handleMouse = () => {
+        console.log("Mouse overrr")
     }
     return (
-        <button onClick={handleClick}>Click ME</button>
+        <button onMouseOver={handleMouse}>Click ME</button>
     )
 }
 
