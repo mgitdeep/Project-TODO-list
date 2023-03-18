d29de70e3cd38cdfd13a2373a1df9a6844d77d54
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-18 11:24:24 +0530

    Can add a single item in the TODO list
    Read the comments to know what are the possibilities that we've tried!

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
 import "../styles/Todo.css";
 
 const Todo = () => {
+
+  const [userInput, setUserInput] = useState('')
+  // const [item1, setitem1] = useState('')         // It can be used to add items one by one - write, click + - repeat
+  const [item1, setitem1] = useState([])
+
+  const handleOnchange = (e) => {
+    // console.log('user input')
+    setUserInput(e.target.value)
+    // console.log(text)
+  }
+  // let i1
+  // const addItem = () => {
+  //   console.log('item added')              Horrible code - never work! - the 1st attempt
+  //   i1 = setitem1(item1)
+  // }
+
+  const addItem = () => {
+    if (!userInput) {
+      alert('Please add an item!')
+    } else {
+      // setitem1(userInput)
+      // setitem1(...item1, userInput)      Square bracs required to hold the previous data
+      setitem1([...item1, userInput])
+    }
+    // setitem1(userInput)                  It'll also work but you can't show the alert
+  }
+
+
+
   return (
 
     <>
@@ -11,18 +41,23 @@ const Todo = () => {
                 <figcaption>Add your list here ✌</figcaption>
             </figure>
             <div className="addItems">
-                <input type="text" placeholder="✍ Add Item" />
+                <input 
+                type="text" 
+                placeholder="✍ Add Item"
+                value={userInput}
+                onChange={handleOnchange}
+                />
 
-                <i className="fa fa-plus add-btn"></i>
+                <i className="fa fa-plus" onClick={addItem}></i>
             </div>
 
             {/* Show our items here */}
             <div className="showItems">
               <div className="eachItem">
-                  <h3>Apple</h3>
+                  <h3>{item1}</h3>
                   <div className="todo-btn">
-                  <i className="fa fa-plus add-btn"></i>
-                  <i className="fa fa-plus add-btn"></i>
+                  <i className="fa fa-edit"></i>
+                  <i className="fa fa-trash-alt"></i>
                   </div>
               </div>
             </div>
