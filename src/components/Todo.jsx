8152ac1994892fcbe512c7c909e776df8f93ca22
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-20 23:02:06 +0530

    Completed Project with comments!
    Explanatory commented codes - make sure to have a look at them

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,7 +4,7 @@ import "../styles/Todo.css";
 // Getting the Local Storage data
 const getLocalData = () => {
   const allData = localStorage.getItem("MyTodoList")
-
+  // console.log(typeof(allData))
   if (allData) {
     return JSON.parse(allData)
   } else {
@@ -16,10 +16,12 @@ const Todo = () => {
 
   const [userInput, setUserInput] = useState('')
   // const [item1, setitem1] = useState('')               It can be used to add items one by one - write, click + - repeat
-  const [items, setitems] = useState(getLocalData())
+  const [items, setItems] = useState(getLocalData())
+  const [editedItem, setEditedItem] = useState('')
+  const [toggleIcon, setToggleIcon] = useState(false)
 
   // console.log(typeof(items))                           Note that "items" is an Object which is holding all the data
-
+  // console.log(items)
   const handleOnchange = (e) => {
     setUserInput(e.target.value)
   }
@@ -34,10 +36,27 @@ const Todo = () => {
   const addItem = () => {
     if (!userInput) {
       alert('Please add an item!')
-    } else {
+    } 
+    // this condition is only for the last editing part
+    else if(userInput && toggleIcon) {
+      setItems(items.map((elm) => {
+        if ( elm.id === editedItem.id ) {
+          return {...elm, name: userInput}
+        }
+        return elm
+      }))
+      setToggleIcon(false)
+    } 
+    else {
+      const myNewInputData = {
+        id: new Date().getTime().toString(),
+        name: userInput
+      }
       // setitem1(userInput)
       // setitem1(...item1, userInput)      Square bracs required to hold the previous data
-      setitems([...items, userInput])
+      // setItems([...items, userInput])    Note what we've changed! We changed "userInput" to "myNewInputData" 
+      setItems([...items, myNewInputData])
+      setToggleIcon(false)
 
       // Adding Local Storage
 
@@ -50,7 +69,7 @@ const Todo = () => {
       // }, [])
       
       
-    }
+    } 
     // setitem1(userInput)                  It'll also work but you can't show the alert
 
     // Clear the item once hit the + button
@@ -63,22 +82,41 @@ const Todo = () => {
         localStorage.setItem("MyTodoList", JSON.stringify(items))                    
       }, [items])
 
+  // Delete the item
   const deleteItem = (curElm) => {
     // console.log(index)
     const updatedItem = items.filter((curElmm) => {
       return curElmm !== curElm
     });
-    setitems(updatedItem)
+    setItems(updatedItem)
   }
 
+  // Remove all items
   const removeAll = () => {
     // let clearAll = items.slice(-1, 0)
-    // setitems(clearAll)
+    // setItems(clearAll)
     // OR
-    setitems([])
+    console.log("By © github.com/mgitdeep")
+    setItems([])
   }
   // console.log(items[1])
   // console.log('remove all')
+
+  // Edit the item
+  const editItem = (curElm) => {
+    const editThisItem = items.find((curElmm) => {
+      // console.log(curElm.name)
+      // console.log(editThisItem)
+      // console.log(curElmm.name)
+      // console.log(curElm)
+      // console.log(curElmm)
+      return curElmm === curElm
+    });
+    // console.log(editThisItem)
+    setUserInput(editThisItem.name) 
+    setToggleIcon(true)
+    setEditedItem(curElm)
+  }
 
 
   return (
@@ -88,7 +126,7 @@ const Todo = () => {
         <div className="child-div">
             <figure>
                 <img src="./images/todo.png" alt="todo" />
-                <figcaption>Add your list here ✌</figcaption>
+                <figcaption>Add your list here 🔰</figcaption>
             </figure>
             <div className="addItems">
                 <input 
@@ -98,18 +136,22 @@ const Todo = () => {
                 onChange={handleOnchange}
                 />
 
-                <i className="fa fa-plus" onClick={addItem}></i>
+                { toggleIcon ? <i className="fa fa-edit" onClick={addItem}></i> : <i className="fa fa-plus" onClick={addItem}></i>}
+                
             </div>
 
             {/* Show our items here */}
             <div className="showItems">
               {items.map( (curElm, index) => {
                 // let x = index;
+                // {console.log(curElm.id)}
                 return (
                   <div className="eachItem" key={index}>
-                    <h3>{curElm}</h3>
+                    {/* <h3>{curElm !== userInput ? curElm : userInput}</h3> */}
+                    <h3>{curElm.name}</h3>
+                    {/* {console.log(index)} */}
                     <div className="todo-btn">
-                    <i className="fa fa-edit"></i>
+                    <i className="fa fa-edit" onClick={() => editItem(curElm)}></i>
                     <i className="fa fa-trash-alt" onClick={() => deleteItem(curElm)}></i>
                     </div>
                 </div>
